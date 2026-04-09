08e8cb7e6648418b6d7f36aef3521d064076a376
oxbingo9 <oxbingo@naver.com>
2026-04-09 16:02:45 +0900

    Update page.js

--- page.js
+++ page.js
@@ -6,14 +6,14 @@ import { getFirestore, collection, query, where, getDocs } from "firebase/firest
 // Firebase 초기화
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBi82idZAraoDMEMVBhVv66tURB0lSI0UM",
-  authDomain: "ledger91-e95ea.firebaseapp.com",
-  databaseURL: "https://ledger91-e95ea-default-rtdb.firebaseio.com",
-  projectId: "ledger91-e95ea",
-  storageBucket: "ledger91-e95ea.firebasestorage.app",
-  messagingSenderId: "747494068723",
-  appId: "1:747494068723:web:67fe836743fb16f89f8286",
-  measurementId: "G-LWQ95J4B1B"
+  apiKey= "AIzaSyBi82idZAraoDMEMVBhVv66tURB0lSI0UM",
+  authDomain= "ledger91-e95ea.firebaseapp.com",
+  databaseURL= "https://ledger91-e95ea-default-rtdb.firebaseio.com",
+  projectId= "ledger91-e95ea",
+  storageBucket= "ledger91-e95ea.firebasestorage.app",
+  messagingSenderId= "747494068723",
+  appId="1:747494068723:web:67fe836743fb16f89f8286",
+  measurementId= "G-LWQ95J4B1B"
 };
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
